70014175d8cc1b6bea201a03ef13a863dfa8cb3e
Jessie James <106570527+jredd2@users.noreply.github.com>
2023-10-17 21:43:26 -0500

    Create awsrek_local.py

---
+++ awsrek_local.py
@@ -0,0 +1,15 @@
+# The main point of this code is to show how to call the Computer Vision API from Python
+
+import os
+
+# use json library to read data passsed back and the web serv
+import sys
+import json
+import pprint
+
+# Hide your keys protect it with your life
+from dotenv import load_dotenv
+load_dotenv(dotenv_path='C:/Users/jessiereddjr./venv/keys.env')
+
+#import the requests library
+import requests
